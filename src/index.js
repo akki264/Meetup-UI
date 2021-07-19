013da42be77a717ff9831d368ec3d65390a22f77
akki264 <akshay_patel26@hotmail.com>
2021-07-19 11:47:13 -0400

    Forgot wrap app with Provider

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./pages/App";
 import "./styles/style.css";
+import { Provider } from 'react-redux';
+import store from "./store";
 // import Register from "./components/Register";
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
